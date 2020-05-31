286470bb6cdd83515b3ed8e1e33c1b1a65fd96ad
Lance Albertson <lance@osuosl.org>
2020-05-30 21:07:59 -0700

    Lock version to 2.0.2 on some aarch64 platforms
    Also update tests to account for that.

--- test/integration/omnibus_spec.rb
+++ test/integration/omnibus_spec.rb
@@ -1,3 +1,25 @@
+case os.family
+when 'redhat'
+  pkgs = %w(curl wget ca-certificates iproute rsync openssh-clients)
+  toolchain_ver = /^1\.1\.109/
+when 'debian'
+  pkgs = %w(curl wget ca-certificates iproute2 rsync openssh-client libssl-dev)
+  case os.arch
+  when x86_64
+    toolchain_ver = /^1\.1\.109/
+  when aarch64
+    toolchain_ver = /^2\.0\.2/
+  end
+when 'suse'
+  pkgs = %w(curl wget iproute2 rsync openssh tar gzip hostname rpm-build)
+  case os.arch
+  when x86_64
+    toolchain_ver = /^1\.1\.109/
+  when aarch64
+    toolchain_ver = /^2\.0\.2/
+  end
+end
+
 describe file '/home/omnibus/load-omnibus-toolchain.sh' do
   its('content') { should_not match /^env/ }
 end
@@ -12,20 +34,11 @@ end
 
 describe package 'omnibus-toolchain' do
   it { should be_installed }
-  its('version') { should match /^1\.1\.109/ }
+  its('version') { should match toolchain_ver }
 end
 
 describe command 'gcc --version' do
   its('exit_status') { should eq 0 }
-end
-
-case os.family
-when 'redhat'
-  pkgs = %w(curl wget ca-certificates iproute rsync openssh-clients)
-when 'debian'
-  pkgs = %w(curl wget ca-certificates iproute2 rsync openssh-client libssl-dev)
-when 'suse'
-  pkgs = %w(curl wget iproute2 rsync openssh tar gzip hostname rpm-build)
 end
 
 pkgs.each do |pkg|
