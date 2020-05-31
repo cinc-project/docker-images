2c84927e7b90a87646f3710196c0927da667c709
Lance Albertson <lance@osuosl.org>
2020-05-31 12:22:58 -0700

    Include common.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -15,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+source scripts/common.sh
 set -e
 
 REPO_ID="$(curl -f -s --header "PRIVATE-TOKEN: ${DEPLOY_TOKEN}" \
